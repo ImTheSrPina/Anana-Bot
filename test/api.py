99eaa39b0a814f61407ee1c762fc1a264c15d4c5
ImTheSrPina <nrusenso2003@gmail.com>
2024-01-29 01:02:08 -0600

    Modifi gitignore

--- test/api.py
+++ test/api.py
@@ -6,7 +6,7 @@ headers = {
     'X-RapidAPI-Key': "dedfbe9631msh93a3fd9874b2042p1a505djsn8a7db06c8e77",
     'X-RapidAPI-Host': "aeona3.p.rapidapi.com"
 }
-
+  
 conn.request("GET", "/?text=hi&userId=12312312312", headers=headers)
 
 res = conn.getresponse()
